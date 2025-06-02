b980cd865323b0afbd1a1ba9c404a3df250e820e
Romans Kolpakovs <108382564+thaumaturg@users.noreply.github.com>
2025-06-02 15:13:25 +0300

    style: convert program.cs to file-scoped namespace

--- backend/Cbo.API/Program.cs
+++ backend/Cbo.API/Program.cs
@@ -4,47 +4,46 @@ using Cbo.API.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
-namespace Cbo.API
+namespace Cbo.API;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        var connectionString = builder.Configuration.GetConnectionString("CboDb")
+            ?? throw new InvalidOperationException("Connection string" + "'CboDb' not found.");
+
+        builder.Services.AddDbContext<CboDbContext>(options =>
+            options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention());
+
+        builder.Services.AddScoped<ITournamentRepository, PostgresTournamentRepository>();
+        builder.Services.AddScoped<ISettingsRepository, PostgresSettingsRepository>();
+        builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            var connectionString = builder.Configuration.GetConnectionString("CboDb")
-                ?? throw new InvalidOperationException("Connection string" + "'CboDb' not found.");
-
-            builder.Services.AddDbContext<CboDbContext>(options =>
-                options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention());
-
-            builder.Services.AddScoped<ITournamentRepository, PostgresTournamentRepository>();
-            builder.Services.AddScoped<ISettingsRepository, PostgresSettingsRepository>();
-            builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapScalarApiReference();
-                app.MapOpenApi();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            app.MapScalarApiReference();
+            app.MapOpenApi();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
