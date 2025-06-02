c1576b536835ae6fa8e81f466a07f03a9892222b
Romans Kolpakovs <108382564+thaumaturg@users.noreply.github.com>
2025-06-02 15:14:26 +0300

    refactor: use explicit types insted of var

--- backend/Cbo.API/Program.cs
+++ backend/Cbo.API/Program.cs
@@ -10,9 +10,9 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-        var connectionString = builder.Configuration.GetConnectionString("CboDb")
+        string connectionString = builder.Configuration.GetConnectionString("CboDb")
             ?? throw new InvalidOperationException("Connection string" + "'CboDb' not found.");
 
         builder.Services.AddDbContext<CboDbContext>(options =>
@@ -28,7 +28,7 @@ public class Program
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
 
-        var app = builder.Build();
+        WebApplication app = builder.Build();
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
