776f1cd898530cf9b84e7fdd65b61edd2065af36
Romans Kolpakovs <108382564+thaumaturg@users.noreply.github.com>
2025-06-06 15:47:54 +0300

    add topics controller with basic CRUD operations; add topics dtos and mappings + dependant questions dtos and mappings

---
+++ backend/Cbo.API/Models/DTO/CreateTopicDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Cbo.API.Models.DTO;
+
+public class CreateTopicDto
+{
+    public required string Title { get; set; }
+    public required int PriorityIndex { get; set; }
+    public required bool IsGuest { get; set; }
+    public required bool IsPlayed { get; set; }
+    public required ICollection<CreateQuestionDto> Questions { get; set; }
+}
---
+++ backend/Cbo.API/Models/DTO/GetQuestionDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Cbo.API.Models.DTO;
+
+public class GetQuestionDto
+{
+    public required int Id { get; set; }
+    public required int QuestionNumber { get; set; }
+    public required int CostPositive { get; set; }
+    public required int CostNegative { get; set; }
+    public required string Text { get; set; }
+    public required string Answer { get; set; }
+    public string? Comment { get; set; }
+    public required int TopicId { get; set; }
+}
