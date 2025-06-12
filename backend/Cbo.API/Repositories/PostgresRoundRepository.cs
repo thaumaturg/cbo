6a62221799f09e5e793ce4d34efd4bf766efe245
Romans Kolpakovs <108382564+thaumaturg@users.noreply.github.com>
2025-06-13 01:51:42 +0300

    add round repository

---
+++ backend/Cbo.API/Repositories/PostgresRoundRepository.cs
@@ -0,0 +1,60 @@
+﻿using Cbo.API.Data;
+using Cbo.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cbo.API.Repositories;
+
+public class PostgresRoundRepository : IRoundRepository
+{
+    private readonly CboDbContext _dbContext;
+
+    public PostgresRoundRepository(CboDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<List<Round>> GetAllAsync()
+    {
+        return await _dbContext.Rounds.ToListAsync();
+    }
+
+    public async Task<Round?> GetByIdAsync(int id)
+    {
+        return await _dbContext.Rounds.FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<Round> CreateAsync(Round round)
+    {
+        await _dbContext.Rounds.AddAsync(round);
+        await _dbContext.SaveChangesAsync();
+        return round;
+    }
+
+    public async Task<Round?> UpdateAsync(int id, Round updatedRound)
+    {
+        Round? existingRound = await _dbContext.Rounds.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (existingRound is null)
+            return null;
+
+        existingRound.TopicId = updatedRound.TopicId;
+        existingRound.MatchId = updatedRound.MatchId;
+
+        await _dbContext.SaveChangesAsync();
+
+        return existingRound;
+    }
+
+    public async Task<Round?> DeleteAsync(int id)
+    {
+        Round? existingRound = await _dbContext.Rounds.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (existingRound is null)
+            return null;
+
+        _dbContext.Rounds.Remove(existingRound);
+        await _dbContext.SaveChangesAsync();
+
+        return existingRound;
+    }
+}
