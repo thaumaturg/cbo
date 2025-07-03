67afdd0dc3c98c7caa484faad5efaf0e369dcf4d
Romans Kolpakovs <108382564+thaumaturg@users.noreply.github.com>
2025-07-04 00:42:30 +0300

    refactor: update Match model properties to be required and initialize collections

--- backend/Cbo.API/Models/Domain/Match.cs
+++ backend/Cbo.API/Models/Domain/Match.cs
@@ -4,14 +4,14 @@ namespace Cbo.API.Models.Domain;
 
 public class Match
 {
-    public int Id { get; set; }
-    public int NumberInTournament { get; set; }
-    public int NumberInStage { get; set; }
-    public TournamentStage CreatedOnStage { get; set; }
-    public Constants.MatchType Type { get; set; }
-    public bool IsFinished { get; set; }
-    public int TournamentId { get; set; }
-    public Tournament Tournament { get; set; }
-    public ICollection<Round> Rounds { get; set; }
-    public ICollection<MatchParticipant> MatchParticipants { get; set; }
+    public required int Id { get; set; }
+    public required int NumberInTournament { get; set; }
+    public required int NumberInStage { get; set; }
+    public required TournamentStage CreatedOnStage { get; set; }
+    public required Constants.MatchType Type { get; set; }
+    public required bool IsFinished { get; set; }
+    public required int TournamentId { get; set; }
+    public required Tournament Tournament { get; set; }
+    public ICollection<Round> Rounds { get; set; } = [];
+    public ICollection<MatchParticipant> MatchParticipants { get; set; } = [];
 }
