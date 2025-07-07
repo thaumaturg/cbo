cc16c15d16f0359e979a10b13b59b77306bd8915
Romans Kolpakovs <108382564+thaumaturg@users.noreply.github.com>
2025-07-08 02:06:29 +0300

    feature: seed roles to CboAuthDbContext

--- backend/Cbo.API/Data/CboAuthDbContext.cs
+++ backend/Cbo.API/Data/CboAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Cbo.API.Data;
@@ -7,5 +8,33 @@ public class CboAuthDbContext : IdentityDbContext
 {
     public CboAuthDbContext(DbContextOptions<CboAuthDbContext> options) : base(options)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        string readerRoleId = "ad040ba3-7725-44e8-b7e0-0d9272be1792";
+        string writerRoleId = "8994bf38-e92b-4fee-a86f-067f012b02f0";
+
+        var roles = new List<IdentityRole>
+        {
+            new IdentityRole
+            {
+                Id = readerRoleId,
+                ConcurrencyStamp = readerRoleId,
+                Name = "Reader",
+                NormalizedName = "Reader".ToUpper()
+            },
+            new IdentityRole
+            {
+                Id = "2",
+                ConcurrencyStamp = writerRoleId,
+                Name = writerRoleId,
+                NormalizedName = "Writer".ToUpper()
+            },
+        };
+
+        modelBuilder.Entity<IdentityRole>().HasData(roles);
     }
 }
