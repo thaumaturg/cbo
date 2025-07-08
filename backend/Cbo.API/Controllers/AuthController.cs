45c796452be716489c884b35c2397b64f106c51a
Romans Kolpakovs <108382564+thaumaturg@users.noreply.github.com>
2025-07-08 21:31:45 +0300

    feature: implement AuthController for user registration with role assignment

---
+++ backend/Cbo.API/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using Cbo.API.Models.DTO;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cbo.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AuthController(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    [HttpPost]
+    [Route("Register")]
+    public async Task<IActionResult> Register([FromBody] CreateUserDto createUserDto)
+    {
+        var identityUser = new IdentityUser
+        {
+            UserName = createUserDto.Username,
+            Email = createUserDto.Email,
+        };
+
+        IdentityResult identityResult = await _userManager.CreateAsync(identityUser, createUserDto.Password);
+
+        if (identityResult.Succeeded)
+        {
+            // Add roles to this User
+            if (createUserDto.Roles is not null && createUserDto.Roles.Length != 0)
+            {
+                identityResult = await _userManager.AddToRolesAsync(identityUser, createUserDto.Roles);
+
+                if (identityResult.Succeeded)
+                {
+                    return Ok("Success! You are now able to log in.");
+                }
+            }
+        }
+
+        return BadRequest("Something went wrong");
+    }
+}
---
+++ backend/Cbo.API/Models/DTO/CreateUserDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cbo.API.Models.DTO;
+
+public class CreateUserDto
+{
+    [DataType(DataType.EmailAddress)]
+    public required string Email { get; set; }
+
+    [DataType(DataType.Password)]
+    public required string Password { get; set; }
+    public required string Username { get; set; }
+    public string? Name { get; set; }
+    public string[]? Roles { get; set; }
+}
