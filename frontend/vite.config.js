04241a2c6b05b446e00a071449f156433782a70b
Romans Kolpakovs <108382564+thaumaturg@users.noreply.github.com>
2025-07-22 15:54:38 +0300

    chore: remove vite-plugin-vue-devtools

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 import fs from 'node:fs'
 
@@ -17,7 +16,7 @@ const httpsSettings = fs.existsSync(developmentCertificateName)
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue()],
   server: {
     https: httpsSettings,
   },
