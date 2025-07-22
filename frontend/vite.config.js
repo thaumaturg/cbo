680a95fc3dcdddd99944248929816e5e8a60414a
Romans Kolpakovs <108382564+thaumaturg@users.noreply.github.com>
2025-07-22 19:06:00 +0300

    chore: add Tailwind CSS dependency

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 import fs from 'node:fs'
 
@@ -16,7 +17,7 @@ const httpsSettings = fs.existsSync(developmentCertificateName)
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   server: {
     https: httpsSettings,
   },
