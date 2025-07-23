d98d278b77c2b308048541f6b7a2fadba79f22a6
Romans Kolpakovs <108382564+thaumaturg@users.noreply.github.com>
2025-07-23 21:17:58 +0300

    chore: edit dummy authentication modal, prepare for validation implementation

--- frontend/src/components/AppAuth.vue
+++ frontend/src/components/AppAuth.vue
@@ -18,7 +18,7 @@ const toggleAuthModal = () => {
 </script>
 
 <template>
-  <Dialog v-model:visible="authModalStore.isOpen" modal header="Edit Profile">
+  <Dialog v-model:visible="authModalStore.isOpen" modal header="Authentication" :draggable="false">
     <Tabs value="0">
       <TabList>
         <Tab value="0">Login</Tab>
@@ -26,42 +26,46 @@ const toggleAuthModal = () => {
       </TabList>
       <TabPanels>
         <TabPanel value="0">
-          <div class="flex items-center gap-4 mb-4">
-            <label for="username" class="font-semibold w-24">Username</label>
-            <InputText id="username" class="flex-auto" autocomplete="off" />
-          </div>
-          <div class="flex items-center gap-4 mb-8">
-            <label for="email" class="font-semibold w-24">Email</label>
-            <InputText id="email" class="flex-auto" autocomplete="off" />
-          </div>
-          <div class="flex justify-end gap-2">
-            <Button type="button" label="Login" @click="toggleAuthModal"></Button>
-          </div>
+          <form>
+            <div class="flex items-center gap-4 mb-4">
+              <label for="loginEmail" class="font-semibold w-24">Email</label>
+              <InputText id="loginEmail" class="flex-auto" autocomplete="off" />
+            </div>
+            <div class="flex items-center gap-4 mb-8">
+              <label for="loginPassword" class="font-semibold w-24">Password</label>
+              <InputText id="loginPassword" type="password" class="flex-auto" autocomplete="off" />
+            </div>
+            <div class="flex justify-end gap-2">
+              <Button type="submit" label="Login" @click="toggleAuthModal"></Button>
+            </div>
+          </form>
         </TabPanel>
         <TabPanel value="1">
-          <div class="flex items-center gap-4 mb-4">
-            <label for="email" class="font-semibold w-24">Email</label>
-            <InputText id="email" class="flex-auto" autocomplete="off" />
-          </div>
-          <div class="flex items-center gap-4 mb-4">
-            <label for="username" class="font-semibold w-24">Username</label>
-            <InputText id="username" class="flex-auto" autocomplete="off" />
-          </div>
-          <div class="flex items-center gap-4 mb-4">
-            <label for="password" class="font-semibold w-24">Password</label>
-            <InputText id="password" class="flex-auto" autocomplete="off" />
-          </div>
-          <div class="flex items-center gap-4 mb-4">
-            <label for="repeatPassword" class="font-semibold w-24">Repeat password</label>
-            <InputText id="repeatPassword" class="flex-auto" autocomplete="off" />
-          </div>
-          <div class="flex items-center gap-4 mb-8">
-            <label for="name" class="font-semibold w-24">Name</label>
-            <InputText id="name" class="flex-auto" autocomplete="off" />
-          </div>
-          <div class="flex justify-end gap-2">
-            <Button type="button" label="Register" @click="toggleAuthModal"></Button>
-          </div>
+          <form>
+            <div class="flex items-center gap-4 mb-4">
+              <label for="registerEmail" class="font-semibold w-24">Email</label>
+              <InputText id="registerEmail" class="flex-auto" autocomplete="off" />
+            </div>
+            <div class="flex items-center gap-4 mb-4">
+              <label for="registerUsername" class="font-semibold w-24">Username</label>
+              <InputText id="registerUsername" class="flex-auto" autocomplete="off" />
+            </div>
+            <div class="flex items-center gap-4 mb-4">
+              <label for="registerFullName" class="font-semibold w-24">Full Name</label>
+              <InputText id="registerFullName" class="flex-auto" autocomplete="off" />
+            </div>
+            <div class="flex items-center gap-4 mb-4">
+              <label for="registerPassword" class="font-semibold w-24">Password</label>
+              <InputText id="registerPassword" type="password" class="flex-auto" autocomplete="off" />
+            </div>
+            <div class="flex items-center gap-4 mb-8">
+              <label for="registerRepeatPassword" class="font-semibold w-24">Repeat password</label>
+              <InputText id="registerRepeatPassword" type="password" class="flex-auto" autocomplete="off" />
+            </div>
+            <div class="flex justify-end gap-2">
+              <Button type="submit" label="Register" @click="toggleAuthModal"></Button>
+            </div>
+          </form>
         </TabPanel>
       </TabPanels>
     </Tabs>
