c1086a446caa8e4132075ab049e27e92d3d3ff95
Romans Kolpakovs <108382564+thaumaturg@users.noreply.github.com>
2025-12-13 15:05:58 +0200

    move TournamentTopic config from data annotations to fluent api

--- backend/Cbo.API/Data/CboDbContext.cs
+++ backend/Cbo.API/Data/CboDbContext.cs
@@ -427,5 +427,46 @@ public class CboDbContext : IdentityDbContext<ApplicationUser, IdentityRole<int>
         });
 
         // TournamentTopic
+        modelBuilder.Entity<TournamentTopic>(entity =>
+        {
+            entity.HasKey(tt => tt.Id);
+
+            entity.Property(tt => tt.Id)
+                .ValueGeneratedOnAdd();
+
+            entity.Property(tt => tt.PriorityIndex)
+                .IsRequired();
+
+            entity.Property(tt => tt.TournamentId)
+                .IsRequired();
+
+            entity.Property(tt => tt.TopicId)
+                .IsRequired();
+
+            entity.Property(tt => tt.TournamentParticipantId)
+                .IsRequired();
+
+            // Many-to-one: TournamentTopic -> Tournament
+            entity.HasOne(tt => tt.Tournament)
+                .WithMany(t => t.TournamentTopics)
+                .HasForeignKey(tt => tt.TournamentId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Many-to-one: TournamentTopic -> Topic
+            entity.HasOne(tt => tt.Topic)
+                .WithMany(t => t.TournamentTopics)
+                .HasForeignKey(tt => tt.TopicId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Many-to-one: TournamentTopic -> TournamentParticipant
+            entity.HasOne(tt => tt.TournamentParticipant)
+                .WithMany(tp => tp.TournamentTopics)
+                .HasForeignKey(tt => tt.TournamentParticipantId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Unique constraint: One topic can only be added once per tournament per participant
+            entity.HasIndex(tt => new { tt.TournamentId, tt.TopicId, tt.TournamentParticipantId })
+                .IsUnique();
+        });
     }
 }
